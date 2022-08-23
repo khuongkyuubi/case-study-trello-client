6c45fa22fad91b79c696b420cd149e0948ec8699
khuongkyuubi <khuong.kyuuib@gmail.com>
2022-08-23 09:27:32 +0700

    create board layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Home from "./pages/Home";
+import Home from "./pages/HomePage/Home";
+import Board from "./pages/BoardPage/Board"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 //dang demo
                 <Route path="/" element={<Home />} />
+                <Route path="/board/:id" element={<Board />} />
             </Routes>
         </BrowserRouter>
     );
