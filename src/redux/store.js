1c9cec1a0c0a694bc7b71fa647fc2f5e918d5a9a
khuongkyuubi <khuong.kyuuib@gmail.com>
2022-08-23 15:48:22 +0700

    create alert slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import boardReducer from "./Slices/boardSlice";
 import listReducer from "./Slices/listSlice";
-
-
+import alertReducer from "./Slices/alertSlice";
 
 const Store = configureStore({
     reducer: {
         board: boardReducer,
-        list: listReducer
-
+        list: listReducer,
+        alert: alertReducer
     }
 })
 
