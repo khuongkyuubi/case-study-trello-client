f6f75326199494eef9e15a731a8a1c9f01791a14
superman <duonga2qp@gmail.com>
2022-08-23 15:43:38 +0700

    Create user redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import boardReducer from "./Slices/boardSlice";
 import listReducer from "./Slices/listSlice";
+import userReducer from "./Slices/userSlice";
 
 
 
 const Store = configureStore({
     reducer: {
         board: boardReducer,
-        list: listReducer
+        list: listReducer,
+        user: userReducer,
+
 
     }
 })
